a99d642f3d5ec4c4bd878c40ba8055296adcaf09
juliogoncalves147 <juliobeites@hotmail.com>
2023-11-17 00:32:16 +0100

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/juliogoncalves147.github.io',
+  base: '/juliogoncalves147.github.io/',
 })
