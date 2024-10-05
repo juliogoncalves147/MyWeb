7bdfa83b187fe6a6166cdc2bd534120197c4fd9b
juliogoncalves147 <juliobeites@hotmail.com>
2024-10-05 01:43:45 +0100

    Small Data Update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -105,7 +105,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() => {
                 const downloadLink = document.createElement('a');
-                downloadLink.href = 'https://juliogoncalves147.github.io/public/CV.pdf';
+                downloadLink.href = 'https://raw.githubusercontent.com/juliogoncalves147/juliogoncalves147.github.io/main/public/CV.pdf';
                 downloadLink.download = 'CV.pdf'; 
                 downloadLink.click();
               }}
